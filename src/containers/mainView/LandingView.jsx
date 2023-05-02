67c221edb3d7529332f6ded7a3d308a2fa41b288
Umer Farooq <umerfarooq.pucit@gmail.com>
2023-05-03 01:21:10 +0500

    added background image in landing view

--- src/containers/mainView/LandingView.jsx
+++ src/containers/mainView/LandingView.jsx
@@ -1,11 +1,35 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { Container, Grid } from "@mui/material";
+import bgImage from "../../spacex-bg.jpg";
 export default function LandingView() {
   const navigate = useNavigate();
   return (
-    <div>
-      Landing View
-      <Button variant="outlined" onClick={() => navigate('allLaunches')}>View Launches</Button>
+    <div
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        width: "100vw",
+        minHeight: "100vh",
+      }}
+    >
+      <Container>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          paddingTop={40}
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Button sx={{ color: 'white', borderColor: "white" }} variant="outlined" onClick={() => navigate("allLaunches")}>
+            View Launches
+          </Button>
+        </Grid>
+      </Container>
     </div>
   );
 }
